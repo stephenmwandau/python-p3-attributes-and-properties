4768b3f2ecccf7a2892151f7f431e479a44967df
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-09-03 20:19:32 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = "Bobby", breed = "Beagle"):
+        self.name = name
+        self.breed = breed
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance (name, str) and 1 < len(name) < 25:
+            self._name = name
+            return self._name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+            return self._breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+fido = Dog(name="Fluffy", breed="Pointer")
+print (fido.name)
+print (fido.breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "Abel Kevin", job = "Research & Development"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 < len(name) < 25:
+            self._name = name.title()
+            return self._name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+            return self._job
+        else:
+            print("Job must be in list of approved jobs.")
+        
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+guido = Person(name="Guido")
+print(guido.name)
+print(guido.job)
